4e86bf5ead3ad6eb803d306b62b8ab7d99c16bf9
noddyz001 <38151895+noddyz001@users.noreply.github.com>
2020-05-15 09:22:01 +0530

    Product Offering Spring Boot Basic Structure

---
+++ product-offering/product-offering/src/main/java/com/productoffering/ProductOfferingApplication.java
@@ -0,0 +1,13 @@
+package com.productoffering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductOfferingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductOfferingApplication.class, args);
+	}
+
+}
---
+++ product-offering/product-offering/src/test/java/com/productoffering/ProductOfferingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productoffering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductOfferingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
